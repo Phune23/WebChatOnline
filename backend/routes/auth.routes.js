17e5d478a80f8383cb6b8fa9228ba1090fc3dd10
Phune23 <phutranbs23@gmail.com>
2024-04-25 23:06:17 +0700

    Auth Routes Setup

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { login,  logout,  signup } from "../controllers/auth.controller.js";   
+
+const router = express.Router();
+
+router.post("/api/auth/signup", signup);
+
+router.post("/api/auth/login", login);
+
+router.post("/api/auth/logout", logout);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 dotenv.config();
@@ -9,5 +12,7 @@ app.get("/", (req, res) => {
     //root route http://localhost:5000/
     res.send("Hello Phu, wellcome to Chat Onlineeeeeeeeeeeeeeeeeeeeeeeeeee!");
 });
+
+app.use("/api/auth/", authRoutes);
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
