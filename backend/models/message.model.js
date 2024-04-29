9745dfd1548c0ae7a5a806acec1932c175f9c1a5
Phune23 <phutranbs23@gmail.com>
2024-04-29 15:27:13 +0700

    Create Message Model

---
+++ backend/models/message.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        require:true,
+    },
+    receiverId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        require:true,
+    },
+    message:{
+        type:String,
+        require:true,
+    }
+}, {timestamps:true} );
