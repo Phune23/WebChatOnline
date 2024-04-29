8a2784985c5728ff4bfe72ad7c4d343c18a0546e
Phune23 <phutranbs23@gmail.com>
2024-04-29 15:16:19 +0700

    Logout Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,6 +83,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-    res.send("logoutUser");
-    console.log("logoutUser");
+    try {
+        res.cookie("jwt","", {maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
 };
