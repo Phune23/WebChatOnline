c40cbba786c694c69ceaf0bbe9b4c2aa8781ce2b
Phune23 <phutranbs23@gmail.com>
2024-04-29 17:43:49 +0700

    Send Message Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(express.json()); //to parse the imcoming requiests with json payloads (f
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 
 // app.get("/", (req, res) => {
 //     //root route http://localhost:5000/
