939b41a72fd54e765168ba5a6022d352468f3c90
Phune23 <phutranbs23@gmail.com>
2024-04-29 15:10:10 +0700

    Login Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -55,9 +55,31 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("loginUser");
-    console.log("loginUser");
+export const login = async (req, res) => {
+    try {
+        const {userName, passWord} = req.body;
+
+        const user = await User.findOne({userName});
+
+        const isPasswordCorrect = await bcrypt.compare(passWord, user?.passWord || "");
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({error:"Invalid userName or passWord"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            passWord: user.passWord,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
 };
 
 export const logout = (req, res) => {
