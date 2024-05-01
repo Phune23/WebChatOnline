27888d3b352d2213af31598adeb89ff16ff98419
Phune23 <phutranbs23@gmail.com>
2024-05-01 17:48:02 +0700

    Logout Functionality

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { SlLogout } from "react-icons/sl";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+
+  const {loading, logout} = useLogout();
   return (
     <div className="mt-auto">
-      <SlLogout className="w-6 h-6 text-white cursor-pointer"/>
+      {!loading ? (
+        <SlLogout className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+        ) : (
+          <span className="loading loading-spinner"></span>
+        )}
     </div>
   );
 };
