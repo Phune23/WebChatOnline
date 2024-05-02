0e937c25d13e36f0dfa30cdf529cd544c4668515
Phune23 <phutranbs23@gmail.com>
2024-05-03 00:23:15 +0700

    Finally Deployment!

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import {app, server} from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the imcoming requiests with json payloads (from req.body)
@@ -20,6 +23,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "indext.html"));
+});
 
 // app.get("/", (req, res) => {
 //     //root route http://localhost:5000/
